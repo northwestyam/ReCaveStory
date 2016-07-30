bb9b0b1426988892ba19d8c7a37f64b5059545a2
Dave Yoon <northwestyam@gmail.com>
2016-07-29 17:44:43 -0700

    Actually added the input class and player class

---
+++ src/input.h
@@ -0,0 +1,24 @@
+#ifndef INPUT_H_
+#define INPUT_H_
+
+#include <SDL/SDL.h>
+#include <map>
+
+class Input {
+public:
+	void beginNewFrame();
+
+	void keyDownEvent(const SDL_Event& event);
+	void keyUpEvent(const SDL_Event& event);
+
+	bool wasKeyPressed(SDLKey key);
+	bool wasKeyReleased(SDLKey key);
+	bool isKeyHeld(SDLKey key);
+
+private:
+	std::map<SDLKey, bool> held_keys_;
+	std::map<SDLKey, bool> pressed_keys_;
+	std::map<SDLKey, bool> released_keys_;
+};
+
+#endif /* INPUT_H_ */
